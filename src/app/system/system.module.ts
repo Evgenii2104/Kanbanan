6032965768e539282a226066df3261d9dab4afb9
evgenii2104 <evgenii2104@rambler.ru>
2024-01-14 17:10:56 +0300

    добовление хедера

--- src/app/system/system.module.ts
+++ src/app/system/system.module.ts
@@ -2,11 +2,15 @@ import {CommonModule} from "@angular/common";
 import {NgModule} from "@angular/core";
 import {SystemComponent} from "./system.component";
 import {SystemRoutingModule} from "./system-routing.module";
+import {MatIconModule} from "@angular/material/icon";
+import {MatToolbarModule} from "@angular/material/toolbar";
 
 @NgModule({
   imports: [
     CommonModule,
-    SystemRoutingModule
+    SystemRoutingModule,
+    MatToolbarModule,
+    MatIconModule
   ],
   declarations: [
     SystemComponent
