7af18204bd6c137b160312fc7c67ce0f3a02a328
evgenii2104 <evgenii2104@rambler.ru>
2024-02-13 22:39:14 +0300

    добавление бордер бокс

---
+++ src/app/system/board/board-task/board.task.component.ts
@@ -0,0 +1,12 @@
+import {Component} from "@angular/core";
+
+
+@Component({
+  selector: 'kb-board-task',
+  templateUrl: 'board.task.component.html',
+  styleUrls: ['board.task.component.scss']
+})
+
+export class BoardTaskComponent {
+
+}
