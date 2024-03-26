7a4b49e873b6b1ba12aee1600ccbb128706ff20d
evgenii2104 <evgenii2104@rambler.ru>
2024-03-27 00:23:39 +0300

    добавление формы

--- src/app/system/board/dialog-overview/dialog.overview.component.ts
+++ src/app/system/board/dialog-overview/dialog.overview.component.ts
@@ -1,13 +1,15 @@
-import {Component, Inject, OnInit} from "@angular/core";
+import {Component, Inject} from "@angular/core";
 import {
   MAT_DIALOG_DATA,
-  MatDialogActions,
-  MatDialogClose, MatDialogContent, MatDialogModule,
-  MatDialogRef,
-  MatDialogTitle
+  MatDialogModule,
+  MatDialogRef
 } from "@angular/material/dialog";
 import { ColumnInterface } from '../../../core/interfaces/column.interface';
 import {MatButtonModule} from "@angular/material/button";
+import {MatInputModule} from "@angular/material/input";
+import {MatSelectModule} from "@angular/material/select";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {FormsModule, NgForm, ReactiveFormsModule} from "@angular/forms";
 
 @Component({
   selector: 'kb-dialog',
@@ -16,13 +18,17 @@ import {MatButtonModule} from "@angular/material/button";
   standalone: true,
   imports: [
     MatDialogModule,
-    //MatFormFieldModule,
-    //MatInputModule,
-    //FormsModule,
-    MatButtonModule
+    MatButtonModule,
+    MatInputModule,
+    MatSelectModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule
   ],
 })
 export class DialogOverviewComponent{
+
+
   constructor(
     public dialogRef: MatDialogRef<DialogOverviewComponent>,
     @Inject(MAT_DIALOG_DATA) public column: ColumnInterface,
@@ -30,10 +36,16 @@ export class DialogOverviewComponent{
 
   onSaveClick(): void {
     this.dialogRef.close('data to save');
+    // @ts-ignore
+    this.submitForm()
   }
 
   onCancelClick(): void {
     this.dialogRef.close('ger');
+  }
+
+  submitForm(form: NgForm){
+    console.log('submitted', form)
   }
 }
 
